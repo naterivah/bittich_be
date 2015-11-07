e2d9bad9d6d8b7248de2e2030a2d9c8517a722cc
Nordine Bittich <contact@bittich.be>
2015-11-07 09:21:30 +0100

    Initial commit

---
+++ src/main/java/be/bittich/website/BittichBeApplication.java
@@ -0,0 +1,12 @@
+package be.bittich.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BittichBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BittichBeApplication.class, args);
+    }
+}
